7d14e92e583a428626b5c6f31cf56ec2449792bb
tconwell <51293949+tconwell@users.noreply.github.com>
2021-06-24 14:41:00 -0500

    Update html5.jl

--- src/html5.jl
+++ src/html5.jl
@@ -1,5 +1,15 @@
 module html5
 
+export a, abbr, address, applet, area, article, aside, audio, b, base, bdi, bdo, blockquote, 
+body, br, button, canvas, caption, cite, code, col, colgroup, command, content, data, datalist,
+dd, del, details, dfn, dialog, div, dl, dt, element, em, embed, eval, fieldset, figcaption, 
+figure, footer, form, h1, h2, h3, h4, h5, h6, head, header, hgroup, hr, html, html5, html_doc, 
+i, iframe, img, include, input, ins, kbd, keygen, label, legend, li, link, main, map, mark, menu,
+menuitem, meta, meter, nav, noscript, object, ol, optgroup, option, output, p, param, picture, 
+pre, progress, q, rb, rp, rt, rtc, ruby, s, samp, script, section, select, shadow, slot, small, 
+source, span, strong, style, sub, summary, sup, table, tbody, td, template, textarea, tfoot, th,
+thead, time, title, tr, track, tt, u, ul, var, video, wbr
+
 html_doc = function(x..., ;doctype::String = "html")
 	return string("<!DOCTYPE ", doctype, ">", join(x))
 end
